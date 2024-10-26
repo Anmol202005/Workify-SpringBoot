24acc7e14f95d5f70f010ffa34fd2a6ba82bc314
Naitik Jain <150283488+naitiikjainn@users.noreply.github.com>
2024-10-26 23:59:54 +0530

    in jwtservices added token generating methods

--- src/main/java/com/workify/auth/service/Jwtservice.java
+++ src/main/java/com/workify/auth/service/Jwtservice.java
@@ -2,19 +2,49 @@ package com.workify.auth.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class Jwtservice {
     private static final String SECRET_KEY = "27bc5821f84be3dca9696869c6248a4bbbf23e30906f1a6c7c2b79ce30e3c32a";
     public String extractusername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    //if you want to generate token using only userdetails
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+    return Jwts
+            .builder()
+            .setClaims(extraClaims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            //the token will be valid till....
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+            //compact() is the method which gonna generate the token....
+            .compact();
+    }
+
     private Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
