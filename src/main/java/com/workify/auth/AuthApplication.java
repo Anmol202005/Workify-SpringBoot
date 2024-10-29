fdb628e54f1114d6bab7d39bd142dcab2f0a3ba8
Anmol202005 <anmol20102005@gmail.com>
2024-10-29 17:47:10 +0530

    configured @EnableJpaRepositories correctly

--- src/main/java/com/workify/auth/AuthApplication.java
+++ src/main/java/com/workify/auth/AuthApplication.java
@@ -2,8 +2,10 @@ package com.workify.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.workify.auth.repository")
 public class AuthApplication {
 
     public static void main(String[] args) {
