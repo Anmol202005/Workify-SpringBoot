bc94aac36b67451a2fefd8bcf8aced480f32b656
Anmol202005 <anmol20102005@gmail.com>
2024-11-01 12:52:19 +0530

    added Cors Config

---
+++ src/main/java/com/workify/auth/config/GlobalCorsConfig.java
@@ -0,0 +1,25 @@
+package com.workify.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://workify-eight.vercel.app/"); // Replace with allowed origin
+        config.addAllowedMethod("*"); // Allows all HTTP methods
+        config.addAllowedHeader("*"); // Allows all headers
+        config.setAllowCredentials(true); // Allows cookies
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
