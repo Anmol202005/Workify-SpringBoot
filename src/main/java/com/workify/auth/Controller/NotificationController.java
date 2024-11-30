676b488f4a67d14fcf518069f30104866995d652
Anmol <anmol20102005@gmail.com>
2024-11-30 20:27:54 +0530

    resolved Auth error

--- src/main/java/com/workify/auth/Controller/NotificationController.java
+++ src/main/java/com/workify/auth/Controller/NotificationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/notification")
 public class NotificationController {
     private final NotificationService notificationService;
 
